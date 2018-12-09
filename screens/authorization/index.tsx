e5327830544c463353dd5ed1fd8579361400bc31
Evgeny Studitskikh <estrueall@gmail.com>
2018-12-09 17:04:37 +0400

    Fake timeout activity indicator implemented

--- screens/authorization/index.tsx
+++ screens/authorization/index.tsx
@@ -7,15 +7,18 @@ import {
   Alert,
   TouchableHighlight,
   Image,
+  ActivityIndicator,
 } from "react-native";
 
 import { styles } from '../../styles';
 import background_pic from '../../assets/background.jpg';
 import logo_pic from '../../assets/logo.png';
+import preload_anim from '../../assets/preload.gif';
 
 interface IAuthorizationScreenState {
   login: string,
-  password: string
+  password: string,
+  isLoading: boolean
 }
 
 export class AuthorizationScreen extends Component<any, IAuthorizationScreenState> {
@@ -24,43 +27,67 @@ export class AuthorizationScreen extends Component<any, IAuthorizationScreenStat
     super(props);
     this.state = {
       login: '',
-      password: ''
+      password: '', 
+      isLoading: true
     };
   }
 
+  componentDidMount = () => setTimeout(() => {
+    this.setState({
+      isLoading: false
+    })
+  }, 3000);
+
   render() {
     
-    const { login, password } = this.state;
+    const { login, password, isLoading } = this.state;
     const isEmpty = !login.length || !password.length;
 
     return (
-      <ImageBackground source={background_pic} style={{width: '100%', height: '100%'}}>
-        <View style={[styles.flex, styles.flexCenter, styles.flexColumn]}>
-          <Image source={logo_pic} style={styles.logo} resizeMode='contain'/>
-          <View>
-            <Text style={styles.label}>Login</Text>
-            <TextInput
-              onChangeText={(value) => this.setState({login: value})}
-              style={styles.input}
-            />
+      isLoading
+        ? <ActivityIndicator 
+            color = '#bc2b78'
+            size = "large"
+            style={{marginTop: 200}}
+          />
+        : <View>
+            <ImageBackground
+              loadingIndicatorSource={preload_anim}
+              source={background_pic} 
+              style={{width: '100%', height: '100%'}}
+            >
+              <View style={[styles.flex, styles.flexCenter, styles.flexColumn]}>
+                <Image 
+                  loadingIndicatorSource={preload_anim}
+                  source={logo_pic}
+                  style={styles.logo}
+                  resizeMode='contain'
+                />
+                <View>
+                  <Text style={styles.label}>Login</Text>
+                  <TextInput
+                    onChangeText={(value) => this.setState({login: value})}
+                    style={styles.input}
+                  />
+                </View>
+                <View style={{marginTop: 10}}>
+                  <Text style={styles.label}>Password</Text>
+                  <TextInput
+                    onChangeText={(value) => this.setState({password: value})}
+                    style={styles.input}
+                    secureTextEntry
+                  />
+                </View>
+                <TouchableHighlight
+                  style={[styles.button, isEmpty ? styles.buttonDisabled : null]}
+                  underlayColor={isEmpty ? '#9CABCA' : '#0D2D5C'}
+                  onPress={() => Alert.alert('Log in!')}
+                >
+                  <Text style={isEmpty ? styles.buttonTextDisabled : styles.buttonText}>LOG IN</Text>
+                </TouchableHighlight>
+              </View>
+            </ImageBackground>
           </View>
-          <View style={{marginTop: 10}}>
-            <Text style={styles.label}>Password</Text>
-            <TextInput
-              onChangeText={(value) => this.setState({password: value})}
-              style={styles.input}
-              secureTextEntry
-            />
-          </View>
-          <TouchableHighlight
-            style={[styles.button, isEmpty ? styles.buttonDisabled : null]}
-            underlayColor={isEmpty ? '#9CABCA' : '#0D2D5C'}
-            onPress={() => Alert.alert('Log in!')}
-          >
-            <Text style={isEmpty ? styles.buttonTextDisabled : styles.buttonText}>LOG IN</Text>
-          </TouchableHighlight>
-        </View>
-      </ImageBackground>
     )
   }
 }
